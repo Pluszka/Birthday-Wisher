e3ff603924ffffe258748824467486fb66b9fbd2
Pliszka <karoliniakkjk@gmail.com>
2022-02-08 23:45:38 +0100

    data file

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import smtplib
+from random import choice
+import datetime as dt
+
+email_day = 1
+current_day_of_week = dt.datetime.now().weekday()
+
+with open('quotes.txt') as file:
+    quotes = file.readlines()
 
 my_email = 'TTe2sTT@gmail.com'
 password = 'day32test.'
-
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs='te1st@yahoo.com',
-                    msg='Subject:Hello\n\n'
-                        'Hello world 3lvl')
-connection.close()
+if email_day == current_day_of_week:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs='te1st@yahoo.com',
+                            msg=f'Subject:Quote\n\n{choice(quotes)}')
