719b09574f86346e2c2d9b3ef0e394b7d4ea7e78
Pliszka <karoliniakkjk@gmail.com>
2022-02-08 22:36:47 +0100

    Hello mail with title

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ password = 'day32test.'
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs='te1st@yahoo.com', msg='Hello')
+connection.sendmail(from_addr=my_email,
+                    to_addrs='te1st@yahoo.com',
+                    msg='Subject:Hello\n\n'
+                        'Hello world 3lvl')
 connection.close()
