0a93d24789e8b0808842027240ff309cdda1c8ae
Pliszka <karoliniakkjk@gmail.com>
2022-02-09 16:06:02 +0100

    send blank mail

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ my_password = 'day32test'
 
 for row in data:
    connection = smtplib.SMTP('smtp.gmail.com')
-
+   connection.starttls()
+   connection.login(user=my_email, password=my_password)
+   connection.sendmail(from_addr=my_email, to_addrs=row['email'],
+                       smg='Happy Birthday\n\n')
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
