95e235efe3110839c119caafc82fa0c41411b987
Pliszka <karoliniakkjk@gmail.com>
2022-02-09 14:16:29 +0100

    data

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
-
+import pandas as pd
+data = pd.read_csv('birthdays.csv')
+print(data)
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
