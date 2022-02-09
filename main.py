109d788a2ad1bd65ef78db55e1b3385b8159ea44
Pliszka <karoliniakkjk@gmail.com>
2022-02-09 14:40:47 +0100

    current day and month variable

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
 import pandas as pd
+import datetime as dt
+
 data = pd.read_csv('birthdays.csv')
 data = data.to_dict(orient='records')
 print(data)
+current_day = dt.datetime.today().day
+current_month = dt.datetime.today().month
+
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
