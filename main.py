90cf6104ee0ee61694b370786da819291a1daaad
Pliszka <karoliniakkjk@gmail.com>
2022-02-09 17:49:33 +0100

    changed signature

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ current_month = dt.datetime.today().month
 
 my_email = 'tte2stt@gmail.com'
 my_password = 'day32test'
+letters = ['letter_templates/letter_1.txt', 'letter_templates/letter_2.txt', 'letter_templates/letter_3.txt']
+wishes = []
+for letter in letters:
+    with open(letter) as file:
+        file = file.read()
+        wishes.append(file)
+print(wishes)
+# for row in data:
+#     connection = smtplib.SMTP('smtp.gmail.com')
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(from_addr=my_email, to_addrs=row['email'], smg='Happy Birthday\n\n')
 
-for row in data:
-   connection = smtplib.SMTP('smtp.gmail.com')
-   connection.starttls()
-   connection.login(user=my_email, password=my_password)
-   connection.sendmail(from_addr=my_email, to_addrs=row['email'],
-                       smg='Happy Birthday\n\n')
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 
