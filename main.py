916f6247d4917391e2b8045cd697f2abea806fba
Pliszka <karoliniakkjk@gmail.com>
2022-02-09 15:55:56 +0100

    connection

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import datetime as dt
+import smtplib
 
 data = pd.read_csv('birthdays.csv')
 data = data.to_dict(orient='records')
 print(data)
 current_day = dt.datetime.today().day
 current_month = dt.datetime.today().month
+
+my_email = 'tte2stt@gmail.com'
+my_password = 'day32test'
+
+for row in data:
+   connection = smtplib.SMTP('smtp.gmail.com')
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
